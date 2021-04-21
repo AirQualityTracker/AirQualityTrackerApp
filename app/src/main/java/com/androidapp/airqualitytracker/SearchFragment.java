9a58bf2c3172a728c34157204e61d49a0dbbc5a8
VelisariosF <fafoutis@csd.auth.gr>
2021-04-21 17:36:19 +0300

    project updated

--- app/src/main/java/com/androidapp/airqualitytracker/SearchFragment.java
+++ app/src/main/java/com/androidapp/airqualitytracker/SearchFragment.java
@@ -2,19 +2,27 @@ package com.androidapp.airqualitytracker;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.SearchView;
+
+import com.google.android.gms.maps.MapView;
 
 
 public class SearchFragment extends Fragment {
-
+    private SearchView searchView;
+    private View sView;
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         return inflater.inflate(R.layout.search_fragment, container, false);
     }
+
+
+
 }
