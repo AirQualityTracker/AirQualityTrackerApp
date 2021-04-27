b0a86b6f438512207a6376802b038d9805367f42
VelisariosF <fafoutis@csd.auth.gr>
2021-04-27 16:47:36 +0300

    project updated

--- app/src/main/java/com/androidapp/airqualitytracker/HomeFragment.java
+++ app/src/main/java/com/androidapp/airqualitytracker/HomeFragment.java
@@ -4,18 +4,37 @@ import android.os.Bundle;
 
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 
-public class HomeFragment extends Fragment {
 
+public class HomeFragment extends Fragment {
+    RecyclerView recyclerView;
+    RecyclerView.LayoutManager layoutManager;
+    RecyclerView.Adapter adapter;
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         return inflater.inflate(R.layout.home_fragment, container, false);
     }
-}
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        recyclerView = (RecyclerView) getView().findViewById(R.id.recycler_view);
+        //Set the layout of the items in the RecyclerView
+        layoutManager = new LinearLayoutManager(getActivity());
+
+        recyclerView.setLayoutManager(layoutManager);
+
+
+        adapter = new RecyclerAdapter();
+        recyclerView.setAdapter(adapter);
+    }
+
+    }
--- app/src/main/java/com/androidapp/airqualitytracker/MainMenuActivity.java
+++ app/src/main/java/com/androidapp/airqualitytracker/MainMenuActivity.java
@@ -2,18 +2,27 @@ package com.androidapp.airqualitytracker;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.PopupMenu;
+import androidx.appcompat.widget.Toolbar;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
+import androidx.navigation.Navigation;
 
+import android.annotation.SuppressLint;
+import android.content.ClipData;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
 import android.view.MenuItem;
+import android.view.View;
 
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.navigation.NavigationView;
 
 public class MainMenuActivity extends AppCompatActivity {
-
+    View topNavView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -21,8 +30,12 @@ public class MainMenuActivity extends AppCompatActivity {
 
 
 
+        //Bottom navigation bar actions
         BottomNavigationView bottomNav = findViewById(R.id.bottom_navigation);
         bottomNav.setOnNavigationItemSelectedListener(navListener);
+
+
+
         //I added this if statement to keep the selected fragment when rotating the device
         if (savedInstanceState == null) {
             getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
@@ -30,17 +43,22 @@ public class MainMenuActivity extends AppCompatActivity {
         }
     }
 
+
+    //Bottom nav bar button listeners
     private BottomNavigationView.OnNavigationItemSelectedListener navListener =
             new BottomNavigationView.OnNavigationItemSelectedListener() {
                 @Override
                 public boolean onNavigationItemSelected(@NonNull MenuItem item) {
                     Fragment selectedFragment = null;
                     switch (item.getItemId()) {
-                        case R.id.nav_home:
+                        case R.id.nav_my_city:
                             selectedFragment = new HomeFragment();
                             break;
                         case R.id.nav_map:
                             selectedFragment = new MapFragment();
+                            break;
+                        case R.id.nav_ranking:
+                            selectedFragment = new RankFragment();
                             break;
                         case R.id.nav_search:
                             selectedFragment = new SearchFragment();
@@ -52,7 +70,37 @@ public class MainMenuActivity extends AppCompatActivity {
                     ftTrans.replace(R.id.fragment_container,
                             selectedFragment);
                     ftTrans.commit();
+
+
                     return true;
                 }
             };
+
+
+
+
+
+
+
+
+
+
+    @SuppressLint("ResourceType")
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        menu.addSubMenu(R.menu.top_navigation_bar_sub_menu);
+        return true;
+    }
+
+
+
+    public void showPopup(View v) {
+        PopupMenu popup = new PopupMenu(this, v);
+        MenuInflater inflater = popup.getMenuInflater();
+        inflater.inflate(R.menu.top_navigation_bar, popup.getMenu());
+        popup.show();
+    }
+
+
+
 }
