3bde672c55bb367e8edf51b3d21c8e7ec93e21ad
VelisariosF <fafoutis@csd.auth.gr>
2021-05-06 12:01:57 +0300

    submenu nav bar created

--- app/src/main/java/com/androidapp/airqualitytracker/GetStartedActivity.java
+++ app/src/main/java/com/androidapp/airqualitytracker/GetStartedActivity.java
@@ -13,7 +13,6 @@ public class GetStartedActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.get_started_activity);
-
     }
 
 
