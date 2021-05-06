1cffeaf40a00b04ce4cbbc8d9e4eeca727a26f88
VelisariosF <fafoutis@csd.auth.gr>
2021-05-06 15:45:20 +0300

    submenu nav bar created

--- app/src/main/java/com/androidapp/airqualitytracker/submenuFragments/AboutAQTFragment.java
+++ app/src/main/java/com/androidapp/airqualitytracker/submenuFragments/AboutAQTFragment.java
@@ -1,11 +1,8 @@
 package com.androidapp.airqualitytracker.submenuFragments;
 
-import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
-import androidx.fragment.app.FragmentManager;
-import androidx.fragment.app.FragmentTransaction;
 
 import android.view.LayoutInflater;
 import android.view.View;
@@ -14,7 +11,7 @@ import android.view.ViewGroup;
 import com.androidapp.airqualitytracker.R;
 
 
-public class SettingsFragment extends Fragment {
+public class AboutAQTFragment extends Fragment {
 
 
 
@@ -29,17 +26,6 @@ public class SettingsFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.search_fragment, container, false);
+        return inflater.inflate(R.layout.about_a_q_t_fragment, container, false);
     }
-
-
-   public View showAbout(){
-      /*  Fragment aboutFragment = new AboutAQTFragment();
-       FragmentManager ftMan = getSupportFragmentManager();
-       FragmentTransaction ftTrans = ftMan.beginTransaction();
-       ftTrans.replace(R.id.fragment_container,
-               aboutFragment);
-       ftTrans.commit();*/
-
-   }
 }
