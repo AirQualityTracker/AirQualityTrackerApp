f17508946554f12fd82e9302e0b7b4a3eff7be7c
VelisariosF <fafoutis@csd.auth.gr>
2021-05-11 15:35:40 +0300

    trying to create navigation ability between fragments in the settings fragment

--- app/src/main/java/com/androidapp/airqualitytracker/submenuFragments/SettingsFragment.java
+++ app/src/main/java/com/androidapp/airqualitytracker/submenuFragments/SettingsFragment.java
@@ -3,6 +3,8 @@ package com.androidapp.airqualitytracker.submenuFragments;
 import android.content.Intent;
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
@@ -10,8 +12,18 @@ import androidx.fragment.app.FragmentTransaction;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.FrameLayout;
+import android.widget.TableLayout;
+import android.widget.TableRow;
+import android.widget.TextView;
 
+import com.androidapp.airqualitytracker.HomeFragment;
+import com.androidapp.airqualitytracker.MapFragment;
 import com.androidapp.airqualitytracker.R;
+import com.androidapp.airqualitytracker.SearchFragment;
+import com.androidapp.airqualitytracker.submenuFragments.settingsFragments.managePlaces.LocationFragment;
+
+import org.jetbrains.annotations.NotNull;
 
 
 public class SettingsFragment extends Fragment {
@@ -23,23 +35,77 @@ public class SettingsFragment extends Fragment {
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
+
+
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.search_fragment, container, false);
+        return inflater.inflate(R.layout.settings_fragment, container, false);
     }
 
+    @Override
+    public void onViewCreated(@NonNull @NotNull View view, @Nullable @org.jetbrains.annotations.Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
 
-   public View showAbout(){
-      /*  Fragment aboutFragment = new AboutAQTFragment();
+
+        TableLayout settingsFragmentList = (TableLayout) view.findViewById(R.id.settingsFragmentList);
+        for(int i = 0; i < settingsFragmentList.getChildCount(); i++) {
+            View view2 = settingsFragmentList.getChildAt(i);
+            if (view2 instanceof TableRow) {
+                // then, you can remove the the row you want...
+                // for instance...
+                TableRow row = (TableRow) view2;
+                row.setOnClickListener(new View.OnClickListener() {
+
+                    @Override
+                    public void onClick(View view) {
+                        Fragment selectedFragment = null;
+                        switch (view.getId()) {
+                            case R.id.managePlaces:
+                                selectedFragment = new LocationFragment();
+                                break;
+                           /* case R.id.:
+                                selectedFragment = new MapFragment();
+                                break;
+                            case R.id.nav_search:
+                                selectedFragment = new SearchFragment();
+                                break;
+                            case R.id.nav_menu:
+                                showPopup();
+                                break;*/
+
+                        }
+                        if(selectedFragment != null) {
+                            FragmentManager ftMan = getParentFragmentManager();
+                            FragmentTransaction ftTrans = ftMan.beginTransaction();
+                            ftTrans.replace(R.id.settingsFragmentList,
+                                    selectedFragment);
+                            ftTrans.commit();
+                        }
+
+
+
+                    }
+                });
+
+            }
+        }
+
+
+    }
+
+    /* public void managePlaces(){
+       Fragment aboutFragment = new AboutAQTFragment();
        FragmentManager ftMan = getSupportFragmentManager();
        FragmentTransaction ftTrans = ftMan.beginTransaction();
        ftTrans.replace(R.id.fragment_container,
                aboutFragment);
-       ftTrans.commit();*/
+       ftTrans.commit();
+     return new View();
 
-   }
+   }*/
 }
---
+++ app/src/main/java/com/androidapp/airqualitytracker/submenuFragments/settingsFragments/managePlaces/MyLocationRecyclerViewAdapter.java
@@ -0,0 +1,63 @@
+package com.androidapp.airqualitytracker.submenuFragments.settingsFragments.managePlaces;
+
+import androidx.recyclerview.widget.RecyclerView;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.androidapp.airqualitytracker.databinding.LocationFragmentBinding;
+import com.androidapp.airqualitytracker.submenuFragments.settingsFragments.managePlaces.placeholder.PlaceholderContent.PlaceholderItem;
+//import com.androidapp.airqualitytracker.submenuFragments.settingsFragments.managePlaces.databinding.LocationFragmentBinding;
+
+import java.util.List;
+
+/**
+ * {@link RecyclerView.Adapter} that can display a {@link PlaceholderItem}.
+ * TODO: Replace the implementation with code for your data type.
+ */
+public class MyLocationRecyclerViewAdapter extends RecyclerView.Adapter<MyLocationRecyclerViewAdapter.ViewHolder> {
+     //users places
+    private final List<PlaceholderItem> mValues;
+
+    public MyLocationRecyclerViewAdapter(List<PlaceholderItem> items) {
+        mValues = items;
+    }
+
+    @Override
+    public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+
+        return new ViewHolder(LocationFragmentBinding.inflate(LayoutInflater.from(parent.getContext()), parent, false));
+
+    }
+
+    @Override
+    public void onBindViewHolder(final ViewHolder holder, int position) {
+        holder.mItem = mValues.get(position);
+        holder.mIdView.setText(mValues.get(position).id);
+        holder.mContentView.setText(mValues.get(position).content);
+    }
+
+    @Override
+    public int getItemCount() {
+        return mValues.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+        public final TextView mIdView;
+        public final TextView mContentView;
+        public PlaceholderItem mItem;
+
+        public ViewHolder(LocationFragmentBinding binding) {
+            super(binding.getRoot());
+            mIdView = binding.itemNumber;
+            mContentView = binding.content;
+        }
+
+        @Override
+        public String toString() {
+            return super.toString() + " '" + mContentView.getText() + "'";
+        }
+    }
+}
